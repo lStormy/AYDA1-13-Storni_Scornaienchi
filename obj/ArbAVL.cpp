849bd9cc36bf7062b7033c1aff7a28c61fbc4592
lStormy <lucio.storni@gmail.com>
2023-10-20 14:02:35 -0300

    Actu

--- obj/ArbAVL.cpp
+++ obj/ArbAVL.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> class Arbavl: {
+template <typename T> class Avl {
     protected:
         class Nodo {
             private:
-                Arbavl * izq;
-                Arbavl * der;
+             Avl * izq;
+             Avl * der;
                 T dato;
             public:
-                Nodo (Arbavl<T> *& izq, Arbavl<T> *& der, const T & elemento) {
+                Nodo  (Avl<T> *& izq, Avl<T> *& der, const T & elemento) {
                     this->dato = elemento;
                     this->izq = izq;
                     this->der = der;
                 }
-                Arbavl<T> * HijoIzq () {return izq;}
-                Arbavl<T> * HijoDer () {return der;}
+             Avl<T> * HijoIzq () {return izq;}
+             Avl<T> * HijoDer () {return der;}
                 const T & raiz () const {return dato;}
         };
-        void vaciar (Arbavl<T> * & raiz);
-        bool balanceado ();
-        void rotar();
+        void vaciar (Avl<T> * raiz);
+        bool balanceado () const;
+        Avl<T> *  rotar(Avl<T> * raiz);
     private: 
         Nodo * primero;
         int altura;
-        void rotar_izq();
-        void rotar_der();
-        void actualizar_alturas(const Arbavl * & raiz);
+        Avl<T> * rotar_izq (Avl<T> * raiz);
+        Avl<T> * rotar_der (Avl<T> * raiz);
+        void actualizar_alturas(const Avl * raiz);
         void insertar (const T & elemento);
     public:
-        Arbavl ();
-        Arbavl (const Arbavl<T> * & otro);
-        ~Arbavl () {vaciar (this);}
-        Arbavl<T> & operator=(const Arbavl<T> * & otro);
-        Arbavl<T> * sub_izq() {return primero->HijoIzq();}
-        Arbavl<T> * sub_der() {return primero->HijoDer();}
+        Avl ();
+        Avl (const Avl<T> *  otro);
+        ~Avl () {vaciar (this);}
+        Avl<T> * operator=(Avl<T> * otro);
+        Avl<T> * sub_izq() {return primero->HijoIzq();}
+        Avl<T> * sub_der() {return primero->HijoDer();}
         const T & dato () const {return primero->raiz();}
         void agregar(const T & elemento) {
             this->insertar(elemento);
@@ -42,22 +42,22 @@ template <typename T> class Arbavl: {
             this->actualizar();
         }
         //void eliminar ();
-        int altura_nodo();
-        bool vacio () {return primero == NULL;}
-        void actualizar();
+        int altura_nodo() const;
+        bool vacio () const {return primero == NULL;}
+        void balancear();
 };
 
-template <typename T> Arbavl<T>::Arbavl() {
+template <typename T> Avl<T>:: Avl() {
     primero = NULL;
     altura = 0;
 }
 
-template <typename T> Arbavl<T>::Arbavl (const Arbavl<T> * & otro) {
+template <typename T> Avl<T>:: Avl(const Avl<T> * otro) {
     altura = otro->altura;
     this->operator=(otro);
 }
 
-template <typename T> void Arbavl<T>::vaciar(Arbavl * & raiz) {
+template <typename T> void Avl<T>::vaciar (Avl * raiz) {
     if (!vacio()) {
         vaciar (raiz->sub_izq());
         vaciar (raiz->sub_der());
@@ -65,20 +65,21 @@ template <typename T> void Arbavl<T>::vaciar(Arbavl * & raiz) {
     }
 }
 
-template <typename T> Arbavl<T> & Arbavl<T>::operator=(const Arbavl * & otro) {
+template <typename T> Avl<T> * Avl<T>::operator=(Avl<T> * otro) {
     if (otro->vacio()) {
         primero = NULL;
         altura = 0;
     }
     else {
-        primero = new Node (otro->sub_izq(), otro->sub_der(), otro->dato());
+        primero = new Nodo (otro->sub_izq(), otro->sub_der(), otro->dato());
         altura = otro->altura;
     }
-    return *this;
+    return this;
 }
 
-template <typename T> int Arbavl<T>::altura_nodo () {
-    if (!vacio()) {S
+
+template <typename T> int Avl<T>::altura_nodo () const {
+    if (!vacio()) {
         if ((!this->sub_izq()->vacio()) || (!this->sub_der()->vacio())) {
             return 1 + max (this->sub_izq()->altura_nodo(),this->sub_der()->altura_nodo());
         }
@@ -89,7 +90,7 @@ template <typename T> int Arbavl<T>::altura_nodo () {
     return 0;
 }
 
-template <typename T> void Arbavl<T>::actualizar_alturas(const Arbavl * & raiz) {
+template <typename T> void Avl<T>::actualizar_alturas(const Avl * raiz) {
     if (!vacio()) {
         altura = this->altura_nodo();
         actualizar_alturas(this->sub_izq());
@@ -100,16 +101,8 @@ template <typename T> void Arbavl<T>::actualizar_alturas(const Arbavl * & raiz) 
     }
 }
 
-int dif (const int & a, const int & b) {
-    if (a < b) {
-        return b - a;
-    } else {
-        return a - b;
-    }
-}
-
-template <typename T> bool Arbavl<T>::balanceado() { //Chequea si un nodo está balanceado.
-    if dif(this->sub_izq()->altura, this->sub_der()->altura) > 1 {
+template <typename T> bool Avl<T>::balanceado() const { //Chequea si un nodo está balanceado.
+    if (((this->sub_izq()->altura - this->sub_der()->altura) > 1) && ((this->sub_izq()->altura - this->sub_der()->altura < -1))) {
         return false;
     }
     else {
@@ -117,36 +110,47 @@ template <typename T> bool Arbavl<T>::balanceado() { //Chequea si un nodo está 
     }
 }
 
-template <typename T> void Arbavl<T>::rotar_izq() {
-    Arbavl * aux (this->sub_der());
-    Arbavl * aux2 (aux->sub_izq());
-    aux->sub_izq() = aux;
-    this->sub_der() = aux2;
+template <typename T> Avl<T> * Avl<T>::rotar_izq (Avl<T> * raiz) {
+    Avl * aux (raiz->sub_der());
+    Avl * aux2 (aux->sub_izq());
+    aux->sub_izq()->operator=( aux);
+    this->sub_der()->operator=( aux2);
     actualizar_alturas(aux);
-    actualizar_alturas(this);
+    actualizar_alturas(raiz);
+    return raiz;
 }
 
-template <typename T> void Arbavl<T>::rotar_der() {
-    Arbavl * aux (this->sub_izq());
-    Arbavl * aux2 (aux->sub_der());
-    aux->sub_der()->operator= (aux);
-    this->sub_izq()->operator= (aux2);
+template <typename T> Avl<T> * Avl<T>::rotar_der (Avl<T> * raiz) {
+    Avl * aux (raiz->sub_izq());
+    Avl * aux2 (aux->sub_der());
+    aux->sub_der()->operator=(aux);
+    raiz->sub_izq()->operator=(aux2);
     actualizar_alturas(aux);
-    actualizar_alturas(this);
+    actualizar_alturas(raiz);
+    return raiz;
 }
 
-template <typename T> void Arbavl<T>::rotar() {
+template <typename T> Avl<T> * Avl <T>::rotar(Avl<T> * raiz) {
+    
     if (!vacio()) {
-        if (sub_izq()->altura > sub_der()->altura) {
-            this->rotar_izq();
+        int balance = (raiz->sub_izq()->altura - raiz->sub_der()->altura);
+        if (balance > 1 && dato() < raiz->sub_izq()->dato()) {
+            raiz = rotar_der(raiz);
+        } else if (balance < -1 && dato() > sub_der()->dato()) {
+            raiz = rotar_izq(raiz);
+        } else if  (balance > 1 && dato() > sub_izq()->dato()){
+            raiz->sub_izq()->operator=(rotar_izq(raiz->sub_izq()));
+            raiz = rotar_der(raiz);            
+        } else {
+            raiz->sub_der()->operator=(rotar_der(sub_der()));
+            raiz = rotar_izq(raiz);
         }
-        else {
-            rotar_der();
-        }
+        
     }
+    return raiz;
 }
 
-template <typename T> void Arbavl<T>::actualizar () {
+template <typename T> void Avl<T>::balancear () {
     if (!vacio()) {
         if (!balanceado()) {
             rotar();
@@ -158,7 +162,7 @@ template <typename T> void Arbavl<T>::actualizar () {
     }
 }
 
-template <typename T> void Arbavl<T>::insertar (const T & elemento) {
+template <typename T> void Avl<T>::insertar (const T & elemento) {
     if (!vacio()) {
         if (elemento > this->dato()) {
             this->sub_der()->insertar(elemento);
@@ -167,8 +171,8 @@ template <typename T> void Arbavl<T>::insertar (const T & elemento) {
         }
     }
     else {
-        Arbavl * izq ();
-        Arbavl * der();
+        Avl * izq = new Avl();
+        Avl * der = new Avl();
         primero = new Nodo (izq, der, elemento);
         actualizar_alturas(this);
     }
@@ -176,8 +180,8 @@ template <typename T> void Arbavl<T>::insertar (const T & elemento) {
 
 
 
-template class Arbavl<int>;
+template class Avl<int>;
 int main () {
-    Arbavl<int> * arb = new Arbavl<int> ();
+    Avl<int> * arb = new Avl<int> ();
        
 }
