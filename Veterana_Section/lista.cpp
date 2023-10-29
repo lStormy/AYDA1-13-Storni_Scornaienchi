b4f0806df48b10bc41c6533168249d41ad73e84d
Christianscorna <christianscornaienchi@gmail.com>
2023-10-29 17:47:47 -0300

    Añadido de encabezado y parametrizacion de clase lista

--- Veterana_Section/lista.cpp
+++ Veterana_Section/lista.cpp
@@ -1,155 +1,176 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include "lista.h"
 using namespace std;
 
-template <typename T> class Lista {
-    protected:
-        struct Nodo {
-            T dato;
-            Nodo * ste;
-        };
-    private:
-        Nodo * primero;
-        Nodo * nuevo (const T & dato) {
-            Nodo * aux = new Nodo();
-            aux->dato = dato;
-            aux->ste = NULL;
-            return aux;
-        }
-        void vaciar (Nodo * lista) {
-            while (lista != NULL) {
-                vaciar (lista->ste);
-                delete lista;
-                lista = NULL;
-            }
-        }
-        void imprimir_lista (Nodo * listita) {
-            cout << endl;
-            Nodo * cursor = listita;
-            while (cursor != NULL) {
-                cout << cursor->dato << endl;
+//Constructoras
+template <typename T>
+Lista<T>::Lista () {
+    primero = NULL;
+}
+
+template <typename T>
+Lista<T>::Lista (const Lista<T> & otro) {
+    primero = NULL;
+    this->operator= (otro);
+}
+
+//Destructoras
+template <typename T>
+Lista<T>::~Lista() {
+    vaciar(this->primero);
+}
+
+template <typename T>
+void Lista<T>::vaciar (Nodo * lista) {
+    while (lista != NULL) {
+        vaciar (lista->ste);
+        delete lista;
+        lista = NULL;
+    }
+}
+
+//Modificadoras
+template <typename T>
+Nodo * Lista<T>::nuevo (const T & dato) {
+    Nodo * aux = new Nodo();
+    aux->dato = dato;
+    aux->ste = NULL;
+    return aux;
+}
+
+template <typename T>
+void Lista<T>::insertar (const T & dato) {
+    if (this->vacio()) {
+        cout << dato << endl;
+        Nodo * aux = nuevo (dato);
+
+        primero = aux;
+    } else {
+        if (dato < primero->dato) {
+            Nodo * aux = nuevo (dato);
+            cout << dato << endl;
+            aux->ste = primero;
+            primero = aux;
+        } else {
+            Nodo * cursor = primero;
+            while ((cursor->ste != NULL) && (cursor->ste->dato < dato)) {
                 cursor = cursor->ste;
             }
-            cout <<endl;
+            Nodo * aux = nuevo(dato);
+            if (cursor->ste != NULL) {
+                aux->ste = cursor->ste;
+            }
+            cursor->ste = aux;
         }
-    public:
-        Lista () {
-            primero = NULL;
-        }
-        Lista (const Lista<T> & otro) {
-            primero = NULL;
-            this->operator= (otro);
-        }
-        ~Lista() {
-            vaciar(this->primero);
-        }
-        void insertar (const T & dato) {
-            if (this->vacio()) {
-                cout << dato << endl;
-                Nodo * aux = nuevo (dato);
+    }
+    imprimir_lista(this->primero);
+}
 
-                primero = aux;
-            } else {
-                if (dato < primero->dato) {
-                    Nodo * aux = nuevo (dato);
-                    cout << dato << endl;
-                    aux->ste = primero;
-                    primero = aux;
-                } else {
-                    Nodo * cursor = primero;
-                    while ((cursor->ste != NULL) && (cursor->ste->dato < dato)) {
-                        cursor = cursor->ste;
-                    }
-                    Nodo * aux = nuevo(dato);
-                    if (cursor->ste != NULL) {
-                        aux->ste = cursor->ste;
-                    }
-                    cursor->ste = aux;
-                }
-            }
-            imprimir_lista(this->primero);
+template <typename T>
+void Lista<T>::eliminar (const int & pos) {
+    assert (pos >= 1 && pos <= len());
+    if (pos == 1) {
+        Nodo * eliminar = primero;
+        primero = primero->ste;
+        delete eliminar;
+    } else {
+        Nodo * cursor = this->primero;
+        int i = 1;
+        while (i < pos-1) {
+            cursor = cursor->ste;
+            i++;
         }
-        bool vacio () const {
-            return this->primero == NULL;
-        }
-        int len() const {
-            if (this->vacio()) {
-                return -1;
-            } else {
-                int i = 1;
-                Nodo * cursor = this->primero->ste;
-                while (cursor != NULL) {
-                    i++;
-                    cursor = cursor->ste;
-                }
-                return i;
-            }
-        }
-        const T & operator()(const int & pos) const {
-            assert (pos >= 1 && pos <= len());
-            Nodo * cursor = this->primero;
-            for (int i = 1; i < pos; i++) {
-                cursor = cursor->ste;
-            }
-            return cursor->dato;
-        }
-        void eliminar (const int & pos) {
-            assert (pos >= 1 && pos <= len());
+        Nodo * eliminar = cursor->ste;
+        cursor->ste = cursor->ste->ste;
+        delete eliminar;
+    }
+}
 
-            if (pos == 1) {
-                Nodo * eliminar = primero;
-                primero = primero->ste;
-                delete eliminar;
-            } else {
-                Nodo * cursor = this->primero;
-                int i = 1;
-                while (i < pos-1) {
-                    cursor = cursor->ste;
-                    i++;
-                }
-                Nodo * eliminar = cursor->ste;
-                cursor->ste = cursor->ste->ste;
-                delete eliminar;
-            }
+//Observadoras
+template <typename T>
+void Lista<T>::imprimir_lista (Nodo * listita) {
+    cout << endl;
+    Nodo * cursor = listita;
+    while (cursor != NULL) {
+        cout << cursor->dato << endl;
+        cursor = cursor->ste;
+    }
+    cout <<endl;
+}
+
+template <typename T>
+bool Lista<T>::vacio () const {
+    return this->primero == NULL;
+}
+
+template <typename T>
+int Lista<T>::len() const {
+    if (this->vacio()) {
+        return -1;
+    } else {
+        int i = 1;
+        Nodo * cursor = this->primero->ste;
+        while (cursor != NULL) {
+            i++;
+            cursor = cursor->ste;
         }
-        Lista<T> & operator= (const Lista<T> & otro) {
-            if (!this->vacio()) {
-                vaciar(this->primero);
-            }
-            if (otro.vacio()) {
-                primero = NULL;
-                primero->ste = NULL;
-            } else {
-                for (int i = 1; i <= otro.len(); i++) {
-                    this->insertar(otro.operator()(i));
-                }
-            }
-            return *this;
+        return i;
+    }
+}
+
+template <typename T>
+Lista<T> & Lista<T>::operator= (const Lista<T> & otro) {
+    if (!this->vacio()) {
+        vaciar(this->primero);
+    }
+    if (otro.vacio()) {
+        primero = NULL;
+        primero->ste = NULL;
+    } else {
+        for (int i = 1; i <= otro.len(); i++) {
+            this->insertar(otro.operator()(i));
         }
-        void mostrar () {
-            cout << endl;
-            for (int i = 1; i <= len(); i++) {
-                cout << this->operator()(i) << " ";
-            }
-            cout << endl;
-        }
-        friend std::ostream& operator << (std::ostream&os, const Lista<T> & l) {
-            os << "[ ";
-            for (int i=1; i <= l.len(); i++) {
-                os << l.operator()(i) << " ";
-            }
-            os << "] ";
-            return os;
-        }
-};
+    }
+    return *this;
+}
+
+template <typename T>
+const T & Lista<T>::operator()(const int & pos) const {
+    assert (pos >= 1 && pos <= len());
+    Nodo * cursor = this->primero;
+    for (int i = 1; i < pos; i++) {
+        cursor = cursor->ste;
+    }
+    return cursor->dato;
+}
+
+template <typename T>
+void Lista<T>::mostrar () {
+    cout << endl;
+    for (int i = 1; i <= len(); i++) {
+        cout << this->operator()(i) << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+friend ostream& operator << (ostream&os, const Lista<T> & l) {
+    os << "[ ";
+    for (int i=1; i <= l.len(); i++) {
+        os << l.operator()(i) << " ";
+    }
+    os << "] ";
+    return os;
+}
 
 template class Lista<int>;
 //template class Lista<string>;
 
-
-void cargar_lista (Lista<int> & list) {
-    cout << "<Deja de cargar con un entero menor a 0>" <<endl;
+template <typename T>
+void Lista<T>::cargar_lista (Lista<int> & list) {
+    cout << "Deja de cargar con un entero menor a 0" <<endl;
     cout << "Inserte un elemento a la lista: ";
     int elemento = -1;
     cin >> elemento;
@@ -160,3 +181,4 @@ void cargar_lista (Lista<int> & list) {
         cin >> elemento;
     }
 }
+//Sukablyand
---
+++ Veterana_Section/lista.h
@@ -0,0 +1,40 @@
+#ifndef LISTA_H_INCLUDED
+#define LISTA_H_INCLUDED
+
+template <typename T>
+class Lista {
+    protected:
+        struct Nodo {
+            T dato;
+            Nodo * ste;
+        };
+
+    private:
+        Nodo * primero;
+        Nodo * nuevo (const T & dato);
+        void vaciar (Nodo * lista);
+        void imprimir_lista (Nodo * listita);
+
+    public:
+        //Constructoras
+        Lista ();
+        Lista (const Lista<T> & otro);
+
+        //Destructora
+        ~Lista();
+
+        //Modificadoras
+        void insertar (const T & dato);
+        void eliminar (const int & pos);
+
+        //Observadoras
+        bool vacio () const;
+        int len() const;
+        const T & operator()(const int & pos) const;
+        Lista<T> & operator= (const Lista<T> & otro) const;
+        void mostrar () const;
+        friend ostream& operator << (ostream&os, const Lista<T> & l) const;
+}
+
+
+#endif // LISTA_H_INCLUDED
