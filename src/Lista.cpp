030068be8c1d71be3482fc0886bc58264bab358e
lStormy <lucio.storni@gmail.com>
2023-11-03 12:31:55 -0300

    Cambios

---
+++ src/Lista.cpp
@@ -0,0 +1,12 @@
+//Source file
+
+#include "Lista.h"
+#include <iostream>
+
+using namespace std;
+
+template <typename T> void Lista<T>::insertar(const T & elemento) {
+    if (lista == NULL) {
+        Lista<T> aux = 
+    }
+}
